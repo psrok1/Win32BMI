103ba5735ac60f6a3658c183fafe5d23c7d22bcc
Pawel Andruszkiewicz <andrusza2@gmail.com>
2016-06-04 17:27:42 +0200

    POPCNT instruction

--- POPCNT.c
+++ POPCNT.c
@@ -8,5 +8,41 @@ int __stdcall POPCNTInstructionEmulator(
 	UNREFERENCED_PARAMETER(instruction);
 	UNREFERENCED_PARAMETER(context);
 
+	unsigned int src;
+	if (instruction.src1 == MEM_32 || instruction.src1 == MEM_16)
+	{
+		src = *(unsigned int*)getEffectiveVA(instruction.mem, context);
+	}
+	else
+	{
+		src = getRegValue(instruction.src1, context);
+	}
+
+	unsigned int operand_size = 32;
+
+	if (instruction.dest & 0x10) // 16-bit instruction
+	{
+		operand_size = 16;
+	}
+
+	unsigned int dest = 0;
+
+	for (unsigned int i = 0; i < operand_size; i++)
+	{
+		if ((src >> i) & 0x1)
+		{
+			++dest;
+		}
+	}
+
+	// Set flags
+	context->flags &= (~FLAG_ZF) & (~FLAG_OF) & (~FLAG_SF) & (~FLAG_AF) & (~FLAG_CF) & (~FLAG_PF);
+	if (src == 0)
+	{
+		context->flags |= FLAG_ZF;
+	}
+
+	setRegValue(instruction.dest, dest, context);
+
 	return TRUE;
 }
